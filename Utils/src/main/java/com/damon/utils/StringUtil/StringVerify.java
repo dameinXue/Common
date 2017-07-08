da3ea41c8fa99a821384c21b2b85f0da619cdb49
damon <jingjingxue9@163.com>
2017-07-08 19:28:39 +0800

    初始化

--- Utils/src/main/java/com/damon/utils/StringUtil/StringVerify.java
+++ Utils/src/main/java/com/damon/utils/StringUtil/StringVerify.java
@@ -1,6 +1,7 @@
 package com.damon.utils.StringUtil;
 
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,13 +18,61 @@ public class StringVerify {
     }
 
     /**
+     * 检测字符串是否为空
+     *
+     * @param string
+     * @return true为空 false不为空
+     */
+    public static boolean isEmpty(String string) {
+        if (null == string || "".equals(string))
+            return true;
+        return false;
+    }
+
+    /**
+     * 检测字符串是否为空
+     *
+     * @param textView
+     * @return true为空 false为为空
+     */
+    public static boolean isEmpty(TextView textView) {
+        if (null == textView || null == textView.getText().toString() || "".equals(textView.getText().toString()))
+            return false;
+        return true;
+    }
+
+    /**
+     * 检测字符串不为空
+     *
+     * @param string
+     * @return true不为空 false为空
+     */
+    public static boolean notEmpty(String string) {
+        if (null == string || "".equals(string))
+            return false;
+        return true;
+    }
+
+    /**
+     * 检测字符串不为空
+     *
+     * @param textView
+     * @return true不为空 false为空
+     */
+    public static boolean notEmpty(TextView textView) {
+        if (null == textView || null == textView.getText().toString() || "".equals(textView.getText().toString()))
+            return false;
+        return true;
+    }
+
+    /**
      * 检测邮箱地址是否合法
      *
      * @param email
      * @return true合法 false不合法
      */
     public static boolean isEmail(String email) {
-        if (null == email || "".equals(email))
+        if (notEmpty(email))
             return false;
         Pattern p = Pattern.compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
         Matcher m = p.matcher(email);
@@ -37,7 +86,7 @@ public class StringVerify {
      * @return true合法 false不合法
      */
     public static boolean isEmail(EditText editText) {
-        if (null == editText || "".equals(editText.getText().toString()))
+        if (notEmpty(editText))
             return false;
         Pattern p = Pattern.compile("\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
         Matcher m = p.matcher(editText.getText().toString());
@@ -51,7 +100,7 @@ public class StringVerify {
      * @return true合法 false不合法
      */
     public static boolean isZipNO(String zipString) {
-        if (null == zipString && "".equals(zipString))
+        if (notEmpty(zipString))
             return false;
         String str = "^[1-9][0-9]{5}$";
         return Pattern.compile(str).matcher(zipString).matches();
@@ -64,10 +113,62 @@ public class StringVerify {
      * @return true合法 false不合法
      */
     public static boolean isZipNO(EditText editText) {
-        if (null == editText && "".equals(editText.getText().toString()))
+        if (notEmpty(editText))
             return false;
         String str = "^[1-9][0-9]{5}$";
         return Pattern.compile(str).matcher(editText.getText().toString()).matches();
     }
+
+    /**
+     * 判断是否大陆手机号码
+     *
+     * @param mobile
+     * @return true合法 false不合法
+     */
+    public static boolean isMobile(String mobile) {
+        if (notEmpty(mobile))
+            return false;
+        String str = "(\\+\\d+)?1[34578]\\d{9}$";
+        return Pattern.compile(str).matcher(mobile).matches();
+    }
+
+    /**
+     * 判断是否大陆手机号码
+     *
+     * @param mobile
+     * @return true合法 false不合法
+     */
+    public static boolean isMobile(EditText mobile) {
+        if (notEmpty(mobile))
+            return false;
+        String str = "(\\+\\d+)?1[34578]\\d{9}$";
+        return Pattern.compile(str).matcher(mobile.getText().toString()).matches();
+    }
+
+
+    /**
+     * 判断是否大陆手机号码
+     *
+     * @param idcard
+     * @return true合法 false不合法
+     */
+    public static boolean isIdCard(String idcard) {
+        if (notEmpty(idcard))
+            return false;
+        return IdcardVerify.IDCardValidate(idcard);
+    }
+
+    /**
+     * 判断是否大陆手机号码
+     *
+     * @param idcard
+     * @return true合法 false不合法
+     */
+    public static boolean isIdCard(EditText idcard) {
+        if (notEmpty(idcard))
+            return false;
+        return IdcardVerify.IDCardValidate(idcard.getText().toString());
+    }
+
 
 }
